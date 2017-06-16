45937ae361212b4b955cd5ff5b3ab5994f581119
Waldek <waldek32-5@o2.pl>
2017-06-16 22:48:23 +0200

    Add empty file if directory with historic program is empty

--- src/main/scala/client/messages/ScalaProgramContainer.scala
+++ src/main/scala/client/messages/ScalaProgramContainer.scala
@@ -34,4 +34,29 @@ object ScalaProgramContainer {
           }
          }""")
   }
+
+  def creators(): (String, String) = {
+    ("creators",
+      """object creators {
+          def main(args: Array[String]) {
+            println("Grupa K. Dudek©\nW składzie:\nBartłomiej Dobosz\nSzymon Furmański\nKamil Dudek\nŁukasz Dudek\nWaldemar Bulanda\nAll rights resered 2017")
+          }
+      }""")
+  }
+
+  def bado() :(String, String) ={
+    ("badoo",
+      """object badoo {
+          import scala.util.Random
+          def main(args: Array[String]){
+            val where = "Gdzie jest Badoo ? Maybe "
+            var positions = Array("Zambia", "Madagascar", "New Zealand")
+            for ( x <- positions ) {
+              println( where + x )
+            }
+            println("Oh! I know !")
+            println("Badoo is in "+Random.shuffle(positions.toList).head)
+          }
+      }""".stripMargin)
+  }
 }
